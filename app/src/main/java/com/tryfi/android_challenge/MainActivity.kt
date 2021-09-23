92745c5eb8adea3dcb9052e89391c665180d61c0
Roum Ines <roum@tryfi.com>
2021-09-23 13:36:33 -0400

    Initial commit

---
+++ app/src/main/java/com/tryfi/android_challenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tryfi.android_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tryfi.android_challenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
